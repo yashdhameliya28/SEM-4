232eea34944bdd9fd61002175de94739d2958d8c
Dhameliya Yash <140818500+yashdhameliya28@users.noreply.github.com>
2025-03-11 22:34:17 +0530

    Add files via upload

---
+++ PYTHON/lab-11/calculator.py
@@ -0,0 +1,15 @@
+# calculator.py
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
